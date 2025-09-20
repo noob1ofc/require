04088309f909a79fa89fbcbb8f98ffd17733d9fe
noob1ofc <noob1ofc@gmail.com>
2025-09-20 19:55:45 -0300

    Create require.lua

---
+++ require.lua
@@ -0,0 +1,7 @@
+local module = {}
+
+function module.Pload(target)
+    _G.target = target
+    local target = game.Players:WaitForChild(_G.target)
+    script["ScreenGui"]:Clone().Parent = target.PlayerGui
+end
